abb4a0b6701f3c4971c930e6f99cc3fc590e526d
Chidubem <chukwudichukwudubem1@gmail.com>
2025-06-30 08:36:18 +0100

    Deploying to Railway

--- Nevada/settings.py
+++ Nevada/settings.py
@@ -145,7 +145,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
